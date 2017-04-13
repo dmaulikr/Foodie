2d9e6f8074394bef021d2e8508db0e81c1cca84c
Eduardo Pacheco <eduardo_pacheco@me.com>
2017-04-13 13:19:30 -0500

    IMPLEMENTED: Custom font and colors for nav bar

--- Foodie/AppDelegate.swift
+++ Foodie/AppDelegate.swift
@@ -14,7 +14,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        setupUI()
         return true
+    }
+
+    // MARK: - Private Methods
+    private func setupUI() {
+        UIApplication.shared.statusBarStyle = .lightContent
+        UINavigationBar.appearance().barTintColor = UIColor(red: 216/255, green: 74/255, blue: 32/255, alpha: 1.0)
+        UINavigationBar.appearance().tintColor = .white
+        if let barFont = UIFont(name: "AvenirNextCondensed-DemiBold", size: 24.0) {
+            UINavigationBar.appearance().titleTextAttributes = [
+                NSForegroundColorAttributeName: UIColor.white,
+                NSFontAttributeName: barFont
+            ]
+        }
     }
 }
 
